95acb3a02dbec01d7e0c63017382386e4f41a765
DK <d.korolyov@gmail.com>
2019-02-12 15:57:18 +0300

    It's the first

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log(4/0);
+console.log("string"*9);
+let something
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person['name']);
+
+let arr = ["plum.png", "orange.jpg", "apple.bpm"];
+
+console.log(arr[2]);
+
+//alert("Hello, world!");
+//let answer = prompt("Yes?", "Yes!");
+
+console.log(typeof(answer));
